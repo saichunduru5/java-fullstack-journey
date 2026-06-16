ac33ee8ad3a43da9447cccfc916f89611ffe4fe0
Chundurusai5 <168450249+saichunduru5@users.noreply.github.com>
2026-06-16 16:42:57 +0530

    Day 1 - Java Basics and Variables

---
+++ Day01_Basics/StudentDetails.java
@@ -0,0 +1,10 @@
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        String name = "Chunduru Sai";
+        int age = 20;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
